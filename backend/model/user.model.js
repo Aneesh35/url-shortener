4b7edd25b98420afcc162cbff4c37351bafe1fe4
user.name like"Aneesh R Rao <user.email like "aneeshrao12@outlook.com>
2025-04-10 08:05:08 +0530

    backend/server-->compeleted

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -14,17 +14,4 @@ const UserSchema = new Schema({
 })
 
 
-UserSchema.statics.hashPassword = async function (password) {
-    return await bcrypt.hash(password, 10);
-
-}
-
-UserSchema.methods.isValidPassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
-}
-
-UserSchema.methods.generateJwt = function () {
-    return jwt.sign({ email: this.email }, secret, { expiresIn: '24h' });
-}
-
 export const userModel = model("users", UserSchema);
