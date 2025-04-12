cacd535432e47aeb4dd98d6352bee690a80f40c9
user.name like"Aneesh R Rao <user.email like "aneeshrao12@outlook.com>
2025-04-12 15:24:00 +0530

    updated frontend -->compeleted

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    react(),
   ],
-})
+  server: {
+    // ✅ This ensures proper routing during local development
+    historyApiFallback: true,
+  },
+  build: {
+    outDir: 'dist',
+  }
+});
